b60f7f05ebd5f91a812101bdc69f242d55015bc3
smurtee <muratees@yahoo.com>
2022-11-02 18:35:59 -0500

    html components added/started header css

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+        <h5>Made by 👽 Sid Muratee</h5>
+        <p>
+            &copy; 2022 SidSpaceSolutions.
+        </p>
+    </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ src/components/myResume.js
@@ -0,0 +1,17 @@
+function MyResume() {
+    return (
+        <section id="my-resume" class="content2">
+            <img src="./android-resumepic.jpg" alt="android-reboot" class="float-left" />
+            <h2>My Resume</h2>
+    
+            <ul>
+                <li>Project 1: https://fe011001.github.io/Doomed-Drinks/</li>
+                <li>Project 2: https://stark-ridge-23340.herokuapp.com/</li>
+            </ul>
+
+        
+        </section>
+    );
+  }
+  
+  export default MyResume;
